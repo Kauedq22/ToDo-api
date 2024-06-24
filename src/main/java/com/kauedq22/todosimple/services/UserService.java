d50e2f4c31b0a0f449c4fa81e50f26ba8ac29001
Kauedq22 <kauedq12@gmail.com>
2024-06-24 15:23:01 -0300

    Created UserService

---
+++ src/main/java/com/kauedq22/todosimple/services/UserService.java
@@ -0,0 +1,52 @@
+package com.kauedq22.todosimple.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kauedq22.todosimple.models.User;
+import com.kauedq22.todosimple.repositories.TaskRepositoy;
+import com.kauedq22.todosimple.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepositoy taskRepositoy;
+
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+                "Usuario nao encontrado! Id:" + id + ", Tipo:" + User.class.getName()));
+    }
+
+    @Transactional
+    public User create(User obj) {
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        this.taskRepositoy.saveAll(obj.getTask());
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to delete related entities");
+        }
+    }
+
+}
